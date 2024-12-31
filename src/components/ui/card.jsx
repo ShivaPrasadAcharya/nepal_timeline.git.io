206c35d82a26e9b9040332bfec12ebf2d8e343b2
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2024-12-31 20:09:27 +0545

    Update card.jsx

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -3,7 +3,7 @@ import * as React from "react"
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={`rounded-lg border bg-card text-card-foreground shadow-sm ${className}`}
+    className={`rounded-lg border bg-card text-card-foreground shadow-sm mb-6 last:mb-0 ${className}`}
     {...props}
   />
 ))
